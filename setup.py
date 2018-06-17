7af3fd1ef3eb4d8f7dd5eb87d936d86dcbf834aa
Lane Rettig <lanerettig@gmail.com>
2018-06-17 19:54:32 -0400

    Add basic setup.py and gitignore

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+
+setup(
+    name='pywebassembly',
+    version='0.1.0',
+    description='PyWebAssembly',
+    author='Paul Dworzanski',
+    author_email='paul@oemm.org',
+    url='https://github.com/poemm/pywebassembly',
+    include_package_data=True,
+    py_modules=[],
+    install_requires=[],
+    license='MIT',
+    zip_safe=False,
+    keywords='ethereum blockchain vm evm wasm ewasm',
+    packages=[],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
